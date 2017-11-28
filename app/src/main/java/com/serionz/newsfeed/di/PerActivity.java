304317e3566f25d6d6031bb4052d0189e0e29aa2
Seremba John <john.seremba@andela.com>
2017-11-28 22:26:24 +0300

    Initialize perActivity interface

--- app/src/main/java/com/serionz/newsfeed/di/PerActivity.java
+++ app/src/main/java/com/serionz/newsfeed/di/PerActivity.java
@@ -1,8 +1,14 @@
 package com.serionz.newsfeed.di;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import javax.inject.Scope;
+
 /**
  * Created by johnpaulseremba on 28/11/2017.
  */
 
+@Scope
+@Retention(RetentionPolicy.RUNTIME)
 public @interface PerActivity {
 }
