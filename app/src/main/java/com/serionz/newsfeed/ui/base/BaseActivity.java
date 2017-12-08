999c670d180afdaefaf1dfeb9e40bce068de5aa5
Seremba John <john.seremba@andela.com>
2017-12-08 10:15:05 +0300

    Set up base fragment and base activity

--- app/src/main/java/com/serionz/newsfeed/ui/base/BaseActivity.java
+++ app/src/main/java/com/serionz/newsfeed/ui/base/BaseActivity.java
@@ -4,14 +4,18 @@ import android.annotation.TargetApi;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 import com.serionz.newsfeed.NewsfeedApplication;
 import com.serionz.newsfeed.di.component.ActivityComponent;
 import com.serionz.newsfeed.di.component.DaggerActivityComponent;
 import com.serionz.newsfeed.di.module.ActivityModule;
+import com.serionz.newsfeed.utils.CommonUtils;
 
 /**
  * Created by johnpaulseremba on 06/12/2017.
@@ -50,23 +54,33 @@ public class BaseActivity extends AppCompatActivity implements MvpView, BaseFrag
 
 	@Override public void showLoading() {
 		hideLoading();
-
+		mProgressDialog = CommonUtils.showDialogLoading(this);
 	}
 
 	@Override public void hideLoading() {
-
+		if (mProgressDialog != null && mProgressDialog.isShowing()) {
+			mProgressDialog.cancel();
+		}
 	}
 
 	@Override public void showMessage(String msg) {
-
+		if (msg != null) {
+			Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+		} else {
+			Toast.makeText(this, "Some error occurred!", Toast.LENGTH_SHORT).show();
+		}
 	}
 
 	@Override public void showMessage(int resId) {
-
+		showMessage(getString(resId));
 	}
 
 	@Override public boolean networkConnected() {
-		return false;
+		ConnectivityManager connectivityManager =
+				(ConnectivityManager) this.getSystemService(CONNECTIVITY_SERVICE);
+		assert connectivityManager != null;
+		NetworkInfo activeNetwork = connectivityManager.getActiveNetworkInfo();
+		return activeNetwork != null && activeNetwork.isConnectedOrConnecting();
 	}
 
 	@Override public void onFragmentAttached() {
